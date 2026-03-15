d095d0aafe80a2f75ad1b97b3cb86afa8fe8042e
Ramzan8-88 <ramzanuiop@gmail.com>
2026-03-16 00:11:23 +0300

    fix

--- include/circle.h
+++ include/circle.h
@@ -3,25 +3,25 @@
 #define INCLUDE_CIRCLE_H_
 
 class Circle {
-private:
-	double radius;
-	double ference;
-	double area;
-	static constexpr double PI = 3.14159265358979323846;
+private: 
+	double radius; 
+	double ference; 
+	double area; 
+	static constexpr double PI = 3.14159265358979323846; 
 
-public:
-	explicit Circle(double r = 0.0);
-	void setRadius(double r);
-	void setFerence(double f);
-	void setArea(double a);
-	double getRadius() const;
-	double getFerence() const;
-	double getArea() const;
+public: 
+	explicit Circle(double r = 0.0); 
+	void setRadius(double r); 
+	void setFerence(double f); 
+	void setArea(double a); 
+	double getRadius() const; 
+	double getFerence() const; 
+	double getArea() const; 
 
-private:
-	void updateFromRadius();
-	void updateFromFerence();
-	void updateFromArea();
+private: 
+	void updateFromRadius(); 
+	void updateFromFerence(); 
+	void updateFromArea(); 
 };
 
 #endif  // INCLUDE_CIRCLE_H_
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,9 @@
 // Copyright 2025 UNN-CS Team
 
-#include <gtest/gtest.h>
+#include "circle.h"           
+#include "tasks.h"            
 
-#include "circle.h"
-#include "tasks.h"
-
+#include <gtest/gtest.h>       
 #include <cmath>
 #include <stdexcept>
 
